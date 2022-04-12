aeabd60089e23f818dc49378f1fcb353c8bc5133
李維京 <asd540680736@outlook.com>
2022-04-12 16:51:28 +0800

    ’0412’

---
+++ pages/about/introduction/index.vue
@@ -0,0 +1,24 @@
+<template>
+      <div>
+<Menu />
+
+
+1212
+      23123
+      </div>
+
+ 
+</template>
+
+<script>
+import Menu from "@/components/commom/head/Menu.vue"
+import Footer from "@/components/commom/foot/Footer.vue"
+
+export default {
+ components: { Menu, Footer },
+}
+</script>
+
+<style>
+
+</style>
